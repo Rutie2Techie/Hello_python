6f64a53c02d833cb846600f29b8b351c226e2f2f
Rutie2Techie <rutujavpatil7@gmail.com>
2021-04-03 23:24:47 +0530

    Len String program

--- StringLen/len_string.py
+++ StringLen/len_string.py
@@ -1,4 +1,12 @@
 # This is program is to count number of characters in string without using len function
 
-A = "This is Ruite"
-print(len(A))
+def len_counter(source_string):
+    count = 0
+    for i in source_string:
+        count += 1
+    return count
+
+
+input_string = "This is Ruite"
+print(len(input_string))
+print(len_counter(input_string))

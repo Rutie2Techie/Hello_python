4039f4b57c19c0d38a56e13de9179dafe3a7404b
Rutie2Techie <rutujavpatil7@gmail.com>
2021-04-20 17:43:05 +0530

    added 2 elements in list

---
+++ Basic_python/list_function.py
@@ -0,0 +1,11 @@
+#Difference between append and extend function
+
+#add 2 elements to a list
+
+
+list1=[23,56,43,87]
+list1.append([65,89])
+print(list1)
+print('----------*----------')
+list1.extend([23,76])
+print(list1)
--- function/factorial.py
+++ function/factorial.py
@@ -1,9 +1,1 @@
-#find factorial of number
 
-def factorial(x):
-     if x<=1:
-         return 1
-     else:
-         return x*factorial(x-1)    #recursive function used here
-num=4
-print("factorial of ",num,"is:",factorial(num))
